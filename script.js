08653e17cdcb56a3e5965539626c46d8749ae18f
colinnnnnnnnnnn <kalinpoiata@gmail.com>
2023-03-26 17:24:29 +0300

    Add script so that squares darken when mouse hovers

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let rows = [];
+let blocks = [];
 const column = document.querySelector('.column');
 
 let n = 10;
@@ -12,8 +13,26 @@ for (let i = 0; i < n; i++) {
     rows[i].setAttribute('class', 'row');
     rows[i].setAttribute('style', 'gap: ' + (100/m) + 'px;');
     for (let j = 0; j < m; j++) {
-        let block = rows[i].appendChild(document.createElement('div'));
+
+        let cover = rows[i].appendChild(document.createElement('div'));
+        let block = cover.appendChild(document.createElement('div'));
         block.setAttribute('class', 'block');
         block.setAttribute('style', 'padding: ' + (300/m) + 'px;');
+        
+        blocks.push(cover);
     }
+}
+
+let brightness = [];
+for (let i = 0; i < blocks.length; i++) {
+    brightness.push(100);
+}
+
+for (let i = 0; i < blocks.length; i++) {
+    blocks[i].addEventListener('mouseover', () => {
+        if (brightness[i] != 0) {
+            brightness[i] -= 10;
+            blocks[i].setAttribute('style', 'filter: brightness(' + brightness[i] + '%);');    
+        }
+    });
 }
